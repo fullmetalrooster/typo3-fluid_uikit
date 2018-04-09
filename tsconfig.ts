be37f9a8854cc407689bc77455502096e23c06f6
Wulf Thimm <w.thimm@service-plus-gmbh.de>
2018-04-09 08:49:13 +0200

    basic changes to fluid_styled_content

---
+++ tsconfig.ts
@@ -0,0 +1,34 @@
+TCEFORM.tt_content {
+  layout.altLabels {
+    1 = Layout 1
+    2 = Layout 2
+    3 = Layout 3
+  }
+  imageorient {
+    #possible values: 0, 1, 2, 8, 9, 10, 17, 18, 25, 26
+    removeItems = 1, 2, 9, 10
+    altLabels {
+    }
+  }
+  header_layout.altLabels {
+    0 = default (h2)
+    1 = h1
+    2 = h2
+    3 = h3
+    4 = h4
+    5 = h5
+  }
+  frame_class {
+    removeItems = ruler-before, ruler-after, indent, indent-left, indent-right
+    addItems {
+      uk-card-default = uk-card-default
+      uk-card-primary = uk-card-primary
+      uk-card-secondary = uk-card-secondary
+      uk-card-hover = uk-card-hover
+    }
+  }
+  header_position {
+    addItems {
+    }
+  }
+}
